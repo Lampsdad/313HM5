73538e7d632e1758979de60410212f2a5f7ca76e
frankenfurth <lampsdad@tamu.edu>
2023-11-05 13:59:26 -0600

    changes to asm file

--- openr.asm
+++ openr.asm
@@ -6,9 +6,14 @@ SYS_OPEN equ 2
 O_RDONLY equ 0
 
 openr:
+    ; Function prologue
     push rbp
     mov rbp, rsp
 
+    ; Arguments:
+    ; [rbp + 16] = path
+
+    ; Open the file
     mov rax, SYS_OPEN
     mov rdi, [rbp + 16]
     mov rsi, O_RDONLY
