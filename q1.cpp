0f7c2e3b43e79c99d01e5543d798fe34eb0d25b4
frankenfurth <lampsdad@tamu.edu>
2023-11-05 14:23:29 -0600

    added aother debug statmetn

--- q1.cpp
+++ q1.cpp
@@ -14,6 +14,7 @@ int main(int argc, char *argv[])
     }
 
     const char *pathname = argv[1];
+    cout << "Pathname: " << pathname << endl;
 
     int fileDescriptor = openr(pathname); // Call the assembly function
     cout << "File descriptor: " << fileDescriptor << endl;
