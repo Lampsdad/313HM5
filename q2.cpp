6f57450dc3b5a4b2ed20cf7a1c76f211bbc398da
frankenfurth <lampsdad@tamu.edu>
2023-11-05 20:11:14 -0600

    Change q2

--- q2.cpp
+++ q2.cpp
@@ -11,9 +11,7 @@
 
 int customFunction() {
     int result = 0;
-    for (int i = 0; i < 100; i++) {
-        result += i;
-    }
+    result += i;
     return result;
 }
 
@@ -32,7 +30,7 @@ double MeasureSystemCallTime(int numIterations) {
         getpid();
     }
     clock_t end = clock();
-    return ((double)(end - start) / CLOCKS_PER_SEC) / numIterations;
+    return ((double)(end - start) / CLOCKS_PER_SEC / numIterations);
 }
 
 int main() {
