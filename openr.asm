ac159571285803be5fd104e9d89e200dc1a24f34
frankenfurth <lampsdad@tamu.edu>
2023-11-05 20:31:35 -0600

    Finalized Files

--- openr.asm
+++
@@ -1,9 +0,0 @@
-global openr
-section .text
-
-openr:
-    mov rax, 2      ; system call number for open()
-    mov rdi, rdi    ; file path
-    xor rsi, rsi    ; flags for read only (O_READ or O_RDONLY)
-    syscall         ; make the system call
-    ret             ; return to caller
